04123cbce007537c7a5f6a5d7d6013ba9925a89a
elifsahin <elif.sahin1@gmx.de>
2013-06-23 18:42:00 +0200

    Create client.java

---
+++ minirestwebservice/client.java
@@ -0,0 +1,17 @@
+package minirestwebservice;
+
+import com.sun.jersey.api.client.*;
+
+
+public class client {
+
+  public static void main(String[] args) {
+		String url = "http://localhost:7160";
+
+	      url = url + "/Quizze";
+
+	      WebResource wrs = Client.create().resource(url);
+
+	      System.out.println( wrs.accept("application/xml").get(String.class));
+	}
+}
